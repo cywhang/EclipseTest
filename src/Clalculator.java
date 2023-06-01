de6102f52542698d4ee5f9b59f6cf9de086af623
hwan <et20062@gmail.com>
2023-06-01 16:36:00 +0900

    최최종

--- src/Clalculator.java
+++ src/Clalculator.java
@@ -3,6 +3,8 @@ public class Clalculator {
 	
 	public long add(long a, long b) {
 		
+		System.out.println("빽다방 프로틴쉐이크");
+		
 		return a + b;
 		
 	}
--- src/Join.java
+++ src/Join.java
@@ -4,8 +4,8 @@ public class Join {
 
 	public void join() {
 		
-		System.out.println("회원가입을 ㅓㅏㅓ합니다.");
-		System.out.println("한글한글");
+		System.out.println("차녕헬스에 오신 것을 환영합니다.");
+		System.out.println("득근득근");
 	}
 
 	public void join2() {
