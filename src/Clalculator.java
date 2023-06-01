1481d0d4f76c56064f1308dd228110e305c17c91
hwan <et20062@gmail.com>
2023-06-01 16:39:44 +0900

    찐최종

--- src/Clalculator.java
+++ src/Clalculator.java
@@ -3,7 +3,7 @@ public class Clalculator {
 	
 	public long add(long a, long b) {
 		
-		System.out.println("빽다방 프로틴쉐이크");
+		System.out.println(" 빽다방 프로틴쉐이크 ");
 		
 		return a + b;
 		
--- src/TestMain.java
+++ src/TestMain.java
@@ -3,7 +3,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		
-		System.out.println("차녕이 이메일 테러범");
+		System.out.println("차녕이 이메일 테러범2");
 		
 	}
 
